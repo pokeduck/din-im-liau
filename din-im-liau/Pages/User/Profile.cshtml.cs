9ed7b0cc59ac1a9a9266daa188e40f18c23a6cb0
pokeduck <40366083+pokeduck@users.noreply.github.com>
2024-06-19 22:44:41 +0800

    Update profile.

--- din-im-liau/Pages/User/Profile.cshtml.cs
+++ din-im-liau/Pages/User/Profile.cshtml.cs
@@ -1,7 +1,9 @@
 
+using System.ComponentModel.DataAnnotations;
 using din_im_liau.Page;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using NUglify.JavaScript.Syntax;
 
 namespace din_im_liau.Pages.User;
@@ -9,7 +11,10 @@ namespace din_im_liau.Pages.User;
 public class ProfileVM
 {
     public int Id { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
+    [StringLength(100, ErrorMessage = "")]
     public string NickName { get; set; }
 }
 public class ProfileModel : BasePageModel
@@ -20,6 +25,21 @@ public class ProfileModel : BasePageModel
 
     public async Task<IActionResult> OnGet()
     {
+        ProfileVM = new ProfileVM
+        {
+            Email = Account?.Email ?? "",
+            NickName = Account?.NickName ?? "",
+            Id = Account?.Id ?? 0
+        };
+        return Page();
+    }
 
+    public async Task<IActionResult> OnPost() {
+        if (ModelState.IsValid) {
+
+        }
+        Console.WriteLine(ProfileVM.NickName);
+        Console.WriteLine(ProfileVM.Email);
+        return Page();
     }
 }

9eb16b2c954cd7f01b7dc0d6b72c01827f9a256f
pokeduck <40366083+pokeduck@users.noreply.github.com>
2024-06-21 01:59:39 +0800

    Update profile.

--- Libraries/Services/AccountService.cs
+++ Libraries/Services/AccountService.cs
@@ -1,9 +1,11 @@
 
 using Amazon.S3.Model;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.DependencyInjection;
 using Models.DataModels;
 using Models.Repositories;
+using Models.ViewModels;
 
 namespace Services;
 
@@ -32,5 +34,17 @@ public class AccountService : BaseService<Account>
         var newAccount = new Account { GoogleOpenId = googleId, NickName = nickName, Email = email, ThumbnailUrl = thumbnailUrl, PermissionId = 2 };
         await Repository.Create(newAccount);
         return newAccount;
+    }
+
+    public async Task UpdateNickName(int id, string nickName)
+    {
+        var account = await Repository.ReadFirstById(id);
+        if (account == null)
+        {
+            return;
+        }
+        account.NickName = nickName;
+
+        await Repository.Update(account);
     }
 }
--- din-im-liau/Pages/User/Profile.cshtml.cs
+++ din-im-liau/Pages/User/Profile.cshtml.cs
@@ -4,7 +4,9 @@ using din_im_liau.Page;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Models.DataModels;
 using NUglify.JavaScript.Syntax;
+using Services;
 
 namespace din_im_liau.Pages.User;
 
@@ -12,9 +14,11 @@ public class ProfileVM
 {
     public int Id { get; set; }
     [Required]
+    [Display(Name = "Email 位址")]
     public string Email { get; set; }
     [Required]
-    [StringLength(100, ErrorMessage = "")]
+    [StringLength(100, ErrorMessage = "AAAAA")]
+    [Display(Name = "您的暱稱")]
     public string NickName { get; set; }
 }
 public class ProfileModel : BasePageModel
@@ -34,12 +38,21 @@ public class ProfileModel : BasePageModel
         return Page();
     }
 
-    public async Task<IActionResult> OnPost() {
-        if (ModelState.IsValid) {
+    public async Task<IActionResult> OnPost()
+    {
+        // if (!ModelState.IsValid)
+        // {
+        //     return Page();
+        // }
+        if (Account != null)
+        {
+            await _accountService.UpdateNickName(Account.Id, ProfileVM.NickName);
+            return RedirectToPage("index");
 
         }
-        Console.WriteLine(ProfileVM.NickName);
-        Console.WriteLine(ProfileVM.Email);
-        return Page();
+        else
+        {
+            return Page();
+        }
     }
 }
